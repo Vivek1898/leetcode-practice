3017ecd071cb4e49e1c7fe581946923766f865fc
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2022-10-11 13:33:38 +0530

    Time: 71 ms (37.10%), Space: 26.5 MB (66.37%) - LeetHub

---
+++ 0581-shortest-unsorted-continuous-subarray/0581-shortest-unsorted-continuous-subarray.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    int findUnsortedSubarray(vector<int>& nums) {
+        int ans=0;
+        int max=nums[0];
+        int end=-1;
+        //Two pointer approach
+        //check from start while we get increasing element
+        for(int i=1;i<nums.size();i++){
+            if(nums[i]<max){
+             end=i;   
+            }else{
+                max=nums[i];
+            }
+        }
+        // Now check from end and store idx
+        int start=0;
+        int min=nums[nums.size()-1];
+        for(int i=nums.size()-2;i>=0;i--){
+              if(nums[i]>min){
+             start=i;  
+            }else{
+                min=nums[i];
+            }
+        }
+        return end-start+1;
+        
+    }
+};
+    
+    
+    
+    
+    
