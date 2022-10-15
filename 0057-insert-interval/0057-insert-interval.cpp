d9cec52582bce9cfdfacdf0f23b4d71e307cd795
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2022-10-15 15:40:10 +0530

    Time: 34 ms (44.73%), Space: 17.7 MB (11.18%) - LeetHub

---
+++ 0057-insert-interval/0057-insert-interval.cpp
@@ -0,0 +1,35 @@
+class Solution {
+public:
+    vector<vector<int>> insert(vector<vector<int>>& intervals, vector<int>& newInterval) {
+             vector<vector<int>> mergedIntervals;
+        if(intervals.size()==0)
+        {
+            intervals.push_back(newInterval);
+             return intervals;
+        }
+           
+        intervals.push_back(newInterval);
+        
+        sort(intervals.begin(),intervals.end());// sort
+        vector<int> pre_interval = intervals[0];
+        // 0 is starting point
+        //1 is ending point
+        
+        //treating as 1d
+        for(auto interval:intervals)
+        {
+            if(interval[0]<=pre_interval[1])// means there is overlapping of intervals
+            {
+                pre_interval[1]=max(interval[1],pre_interval[1]); // need to check which has max ending points hence we will make ending point of pre_interval to max
+            }
+            else
+            { // if there is no overlapping
+                mergedIntervals.push_back(pre_interval); // simplely push pre-interval to mergedintervals vector
+                pre_interval=interval;
+            }     
+        }
+        
+        mergedIntervals.push_back(pre_interval);
+        return mergedIntervals;
+    }
+};
