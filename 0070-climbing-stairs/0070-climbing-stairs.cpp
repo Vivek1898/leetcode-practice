a9858e2bed486143b59190010b35da6145c841c2
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2022-10-27 19:16:31 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (56.47%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    //Move 1 and 2
+    //recursive
+    void helper(int n,int &ans){
+        if(n==0){
+            ans+=1;
+        }
+        if(n<0) return;
+        
+        helper(n-1,ans);
+        
+         helper(n-2,ans);
+ }
+    
+    
+    int climbStairs2(int n) {
+        int ans=0;
+        
+        helper(n,ans);
+        
+      return ans;
+        
+    }
+    
+    
+      int climbStairs(int n) {
+        int arr[n+1];
+           arr[0]=1;
+          arr[1]=1;
+          
+          for(int i=2;i<=n;i++){
+              arr[i]=arr[i-1]+arr[i-2];
+              
+          }
+        
+       
+        
+      return arr[n];
+        
+    }
+   
+};
