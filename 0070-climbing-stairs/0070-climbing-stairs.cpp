d9496e121f756ae41d326641f887d7e96da892a1
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2022-10-30 23:11:32 +0530

    Time: 0 ms (100.00%), Space: 5.9 MB (56.52%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.cpp
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -30,11 +30,8 @@ public:
           arr[1]=1;
           
           for(int i=2;i<=n;i++){
-              arr[i]=arr[i-1]+arr[i-2];
-              
+              arr[i]=arr[i-1]+arr[i-2];   
           }
-        
-       
         
       return arr[n];
         
