20652c80a497117dd6303f54d36325a413e434ce
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2022-10-31 18:57:19 +0530

    Time: 37 ms (32.49%), Space: 13.9 MB (41.82%) - LeetHub

---
+++ 0740-delete-and-earn/0740-delete-and-earn.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int deleteAndEarn(vector<int>& nums) {
+        int n = 10001;
+        vector<int> count(n);
+        
+        for(auto &x : nums) count[x] += x;
+
+        for(int i = 2; i < n; ++i) 
+            count[i] = max(count[i] + count[i - 2], count[i - 1]);
+        
+        return count[n - 1];
+    }
+};
