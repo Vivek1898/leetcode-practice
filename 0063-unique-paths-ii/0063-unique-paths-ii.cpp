a6ff84bfc4d13793ec9958f140b6bcdccdf160db
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2022-11-26 02:24:57 +0530

    Time: 7 ms (50.49%), Space: 8 MB (14.39%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -10,9 +10,25 @@ public:
         if(y>0) ans += fun(x, y-1, grid);
         return dp[x][y] = ans;
     }
-    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+    int uniquePathsWithObstaclesMemo(vector<vector<int>>& grid) {
         int m = grid.size(), n = grid[0].size();   
         dp.resize(m, vector<int>(n, -1));
         return fun(m-1, n-1, grid);
+    }
+     int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        if(grid[0][0]==1) return 0;
+        int m = grid.size(), n = grid[0].size();   
+        vector<vector<int>> dp(m, vector<int>(n));
+        dp[0][0] = 1;
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(i==0 && j==0) continue;
+                dp[i][j] = 0;
+                if(grid[i][j]==1) continue;
+                if(i>0) dp[i][j] += dp[i-1][j];
+                if(j>0) dp[i][j] += dp[i][j-1];
+            }
+        }
+        return dp[m-1][n-1];
     }
 };
