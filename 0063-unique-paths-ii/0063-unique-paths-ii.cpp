0ee59fe7a75229140598f86ce39f519cd010001b
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2022-11-26 02:23:44 +0530

    Time: 7 ms (50.49%), Space: 7.9 MB (14.39%) - LeetHub

--- 0063-unique-paths-ii/0063-unique-paths-ii.cpp
+++ 0063-unique-paths-ii/0063-unique-paths-ii.cpp
@@ -1,13 +1,18 @@
 class Solution {
 public:
-    int uniquePathsWithObstacles(vector<vector<int> > &obstacleGrid) {
-        int m = obstacleGrid.size() , n = obstacleGrid[0].size();
-        vector<vector<int>> dp(m+1,vector<int>(n+1,0));
-        dp[0][1] = 1;
-        for(int i = 1 ; i <= m ; ++i)
-            for(int j = 1 ; j <= n ; ++j)
-                if(!obstacleGrid[i-1][j-1])
-                    dp[i][j] = dp[i-1][j]+dp[i][j-1];
-        return dp[m][n];
+    vector<vector<int>> dp;
+    int fun(int x, int y, vector<vector<int>>& grid){
+        if(grid[x][y]==1) return 0;
+        if(x==0 && y==0) return 1;
+        if(dp[x][y]!=-1) return dp[x][y];
+        int ans = 0;
+        if(x>0) ans += fun(x-1, y, grid);
+        if(y>0) ans += fun(x, y-1, grid);
+        return dp[x][y] = ans;
+    }
+    int uniquePathsWithObstacles(vector<vector<int>>& grid) {
+        int m = grid.size(), n = grid[0].size();   
+        dp.resize(m, vector<int>(n, -1));
+        return fun(m-1, n-1, grid);
     }
 };
