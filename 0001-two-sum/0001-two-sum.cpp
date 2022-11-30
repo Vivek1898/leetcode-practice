c27e0cb96107843bec528eb4db1aa4b9d3699d89
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2022-12-01 02:05:32 +0530

    Time: 11 ms (95.30%), Space: 11.2 MB (19.16%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    
+          map<int, int> umap;
+        int difference;
+
+        for(int i = 0; i < nums.size(); i++ ){
+            difference = target - nums[i];
+            if(umap.find(difference) != umap.end()) {
+                vector<int> v{umap[difference], i};
+                return v;
+            } else {
+                umap[nums[i]] = i;
+            }
+        }
+
+        return vector<int> {};
+    }
+};
