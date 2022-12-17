7e82a49b53623ddc4255904af6502144c0a053b2
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2022-12-17 23:45:49 +0530

    Time: 3 ms (84.47%), Space: 7.4 MB (96.10%) - LeetHub

--- 0090-subsets-ii/0090-subsets-ii.cpp
+++ 0090-subsets-ii/0090-subsets-ii.cpp
@@ -1,6 +1,6 @@
 
-
-class Solution {
+//Recursion
+class Solution2 {
 public:
     void helper(vector<int>& nums,  vector<int> &curr,vector<vector<int>> &ans,int idx){
       //  if(idx==nums.size()) {
@@ -29,5 +29,37 @@ public:
         helper(nums,curr,ans,0);
         return ans;
         
+    }
+};
+
+
+// bfs
+
+
+class Solution {
+public:
+    vector<vector<int>> subsetsWithDup(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+         vector<vector<int>>powerset;
+        powerset.push_back({});
+        int startIdx=0,endIdx=0;
+        // if duplicte
+        // then start from last genreted subset
+        for(int i=0;i<nums.size();i++){
+            startIdx=0;
+            
+            if(i>0 and nums[i]==nums[i-1]){
+                startIdx=endIdx+1;
+            }
+            
+            endIdx=powerset.size()-1;
+            for(int j=startIdx;j<=endIdx;j++){
+                  vector<int>copy(powerset[j]);
+                    copy.push_back(nums[i]);
+                        powerset.push_back(copy);
+            }
+            
+        }
+        return powerset;
     }
 };
