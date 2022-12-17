48e93537ca93a400c30f1cede34ed38f35ce79f6
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2022-12-17 12:48:02 +0530

    Time: 4 ms (67.12%), Space: 8 MB (29.60%) - LeetHub

--- 0046-permutations/0046-permutations.cpp
+++ 0046-permutations/0046-permutations.cpp
@@ -1,13 +1,75 @@
-class Solution {
-public:
+//bfs
+
+// class Solution {
+// public:
+//     vector<vector<int>> permute(vector<int>& nums) {
+//         vector<vector<int>>res;
+//         queue<vector<int>>permut;
+//         permut.push(vector<int>());
+        
+//         for(auto curr:nums){
+//             int n=permut.size();
+//             for(int i=0;i<n;i++){
+//                 vector<int>oldPermut=permut.front();
+//                 permut.pop();
+//                 for(int j=0;j<=oldPermut.size();j++){
+//                       vector<int>newPermut(oldPermut);
+//                       newPermut.insert(newPermut.begin()+j,curr);
+                    
+//                     if(newPermut.size()==nums.size()){
+//                         res.push_back(newPermut);
+//                     }else{
+//                          permut.push(newPermut);
+//                     }
+//                 }
+//             }
+//         }
+//         return res;
+        
+//     }
+// };
+
+//stl
+
+
+// class Solution {
+// public:
      
     
+//     vector<vector<int>> permute(vector<int>& nums) {
+//        vector<vector<int>> res;
+//         sort(nums.begin(), nums.end());
+//         do{
+//             res.push_back(nums);
+//         }while(next_permutation(nums.begin(),nums.end()));
+//         return res;   
+//     }
+    
+  
+// };
+
+
+class Solution {
+public:
+      void helper(vector<int>& nums,int idx,vector<int>& currPerm,vector<vector<int>>& res){
+         if(idx == nums.size()){
+            res.push_back(currPerm);
+            return ;
+        }else{
+             // ADD CURRENT NO AT EVERY POSITION
+             for(int i=0;i<=currPerm.size();i++)
+             {
+                vector<int> newPerm(currPerm);
+                newPerm.insert(newPerm.begin()+i,nums[idx]); 
+                 helper(nums,idx+1,newPerm,res);
+             }
+         }
+    }  
+    
     vector<vector<int>> permute(vector<int>& nums) {
-       vector<vector<int>> res;
-        sort(nums.begin(), nums.end());
-        do{
-            res.push_back(nums);
-        }while(next_permutation(nums.begin(),nums.end()));
+        vector<vector<int>>res;
+         vector<int> currPerm;
+        helper(nums,0,currPerm,res);
         return res;   
     }
     
