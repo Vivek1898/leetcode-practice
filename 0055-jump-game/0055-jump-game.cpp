3ab268a74793a049e8003e35ff90a37b98c03e3f
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2022-12-26 19:13:20 +0530

    Time: 583 ms (22.20%), Space: 48.3 MB (94.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.cpp
+++ 0055-jump-game/0055-jump-game.cpp
@@ -5,7 +5,7 @@ public:
     int goodPos = n - 1;
         cout<<n<<" ->Goosd"<<endl;
         cout<<n-2<<" ->sz"<<endl;
-    // it over 2nd last position
+    // it over second last position
     for (int i = n - 2; i >= 0; i--)
     {
       // Logic
