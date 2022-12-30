92708e23e21250a3cda90318207c3d9a721d0c62
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2022-12-30 10:12:59 +0530

    Time: 18 ms (76.04%), Space: 14.6 MB (39.10%) - LeetHub

--- 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
+++ 0797-all-paths-from-source-to-target/0797-all-paths-from-source-to-target.cpp
@@ -1,4 +1,4 @@
-class Solution {
+class SolutionDFS {
 public:
     void dfs(vector<vector<int>>& graph,vector<vector<int>>& ans,vector<int>& currAns,int curr){
         currAns.push_back(curr);
@@ -14,6 +14,29 @@ public:
         vector<vector<int>> ans;
         vector<int> currAns;
         dfs(graph,ans,currAns,0);
+        return ans;
+    }
+};
+
+class Solution {
+public:
+    vector<vector<int>> allPathsSourceTarget(vector<vector<int>>& graph) {
+        vector<vector<int>> ans;
+        queue<vector<int>> q;
+        q.push({0});
+        while(!q.empty()){
+            auto topElement=q.front();
+            q.pop();
+            for(auto i:graph[topElement.back()]){
+                topElement.push_back(i);
+                if(i==graph.size()-1)
+                    ans.push_back(topElement);
+                else{
+                    q.push(topElement);
+                }
+                topElement.pop_back();
+            }
+        }
         return ans;
     }
 };
