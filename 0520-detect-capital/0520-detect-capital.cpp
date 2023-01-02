88aa6e161ade8eff67154b5f461c6ac32155644b
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2023-01-02 23:04:01 +0530

    Time: 6 ms (13.20%), Space: 6.1 MB (70.23%) - LeetHub

---
+++ 0520-detect-capital/0520-detect-capital.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    bool detectCapitalUse(string word) {
+
+        int countCapitals = 0;
+
+        for(char c : word ){
+            if(isupper(c))
+                countCapitals++;
+        }
+
+        return (countCapitals == 0) || (countCapitals == word.size()) || (countCapitals == 1 && isupper(word[0]));
+        
+    }
+};
