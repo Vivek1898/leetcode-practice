73ea34f23db3dac69404ee111bf9376068909414
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2023-01-11 02:07:01 +0530

    Time: 7 ms (56.94%), Space: 6.7 MB (28.30%) - LeetHub

---
+++ 0052-n-queens-ii/0052-n-queens-ii.cpp
@@ -0,0 +1,57 @@
+class Solution {
+public:
+        bool isSafe1(int row,int col,vector < string > & board, vector < vector < string >> & ans, int n){
+         int duprow = row;
+      int dupcol = col;
+        while(row>=0 and col>=0){
+            if(board[row--][col--]=='Q'){
+                return false;
+            }
+            
+        }
+          col = dupcol;
+      row = duprow;
+        while(col>=0){
+              if(board[row][col--]=='Q'){
+                return false;
+            }
+        }
+          col = dupcol;
+      row = duprow;
+         while(row<n and col>=0){
+            if(board[row++][col--]=='Q'){
+                return false;
+            }
+            
+        }
+          return true;
+    }
+    
+     void solve(int col, vector < string > & board, vector < vector < string >> & ans, int n) {
+      if (col == n) {
+        ans.push_back(board);
+        return;
+      }
+      for (int row = 0; row < n; row++) {
+        if (isSafe1(row, col, board,ans, n)) {
+          board[row][col] = 'Q';
+          solve(col + 1, board, ans, n);
+          board[row][col] = '.';
+        }
+      }
+    }
+    int solveNQueens(int n) {
+    vector < vector < string >> ans;
+      vector < string > board(n);
+     string s(n, '.');
+      for (int i = 0; i < n; i++) {
+        board[i] = s;
+      }
+      solve(0, board, ans, n);
+    //cout<<ans.size();
+      return ans.size();
+    }
+    int totalNQueens(int n) {
+        return solveNQueens(n);
+    }
+};
