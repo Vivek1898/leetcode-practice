82958fa850ac32db60e2c455a7f0b401cd46e2c3
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2023-01-12 10:58:27 +0530

    Time: 39 ms (86.46%), Space: 28.2 MB (56.35%) - LeetHub

--- 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
+++ 0209-minimum-size-subarray-sum/0209-minimum-size-subarray-sum.cpp
@@ -5,13 +5,15 @@ public:
         
         for(int fast=0;fast<nums.size();fast++){
             sum+=nums[fast];
-            
+         
             while(sum>=target){
                 minLen=min(minLen,fast-slow+1);
-                //Exclude
-                sum-=nums[slow];
-                slow++;
+                sum-=nums[slow++];
+              
             }
+                
+              //slow--;
+            
         }
         //No valiad
         if(minLen == INT_MAX) return 0;
