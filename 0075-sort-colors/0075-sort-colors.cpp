09b7a4768670d19326acb172d62e53eec55a12f7
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2023-01-26 00:24:39 +0530

    Time: 4 ms (31.80%), Space: 8.2 MB (73.04%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,14 +1,13 @@
 class Solution {
 public:
     void sortColors(vector<int>& a) {
-        // quick sort algo with pivot 1
-        int p = 1;
+       
         int l = 0, r = a.size() - 1;
         for(int i = 0; i <= r; i++) {
-            if(a[i] < p) {
+            if(a[i] == 0) {
                 swap(a[i], a[l]);
                 l++;
-            } else if(a[i] > p) {
+            } else if(a[i] ==2 ) {
                 swap(a[i], a[r]);
                 r--;
                 i--;
