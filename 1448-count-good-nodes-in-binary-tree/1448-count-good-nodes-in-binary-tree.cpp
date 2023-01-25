b49adc5220ab4a4e0f24c7cf6a1b8681d16867f7
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2023-01-25 14:58:49 +0530

    Time: 194 ms (34.26%), Space: 86.2 MB (92.84%) - LeetHub

--- 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
+++ 1448-count-good-nodes-in-binary-tree/1448-count-good-nodes-in-binary-tree.cpp
@@ -9,7 +9,38 @@
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
  */
+
 class Solution {
+public:
+    int count=0;
+     
+    int solve(TreeNode* root,int mx,int &cnt){
+        if(!root) return 0 ;
+        
+      if(root->val>=mx){
+          cnt++;
+          cout<<cnt<<endl;
+          mx=root->val;
+      }
+       // mx=max(mx,root->val);
+        
+        
+   int a=   solve(root->left,mx,cnt);
+    int b=   solve(root->right,mx,cnt);
+        return cnt;
+        
+     //   return (a+b+mx<=root->val);
+        
+    }
+    int goodNodes(TreeNode* root) {
+     //  int mx=INT_MIN;
+       // int count=0;
+        int cnt=0;
+        return solve(root,root->val,cnt);;
+            
+    }
+};
+class Solution1 {
 public:
     int count=0;
      
