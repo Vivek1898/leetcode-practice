0cb80ac3b4dd542e6ad75fbbdb4cb6ff480298b8
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2023-01-26 00:33:16 +0530

    Time: 7 ms (8.59%), Space: 8.4 MB (29.18%) - LeetHub

--- 0075-sort-colors/0075-sort-colors.cpp
+++ 0075-sort-colors/0075-sort-colors.cpp
@@ -1,16 +1,17 @@
 class Solution {
 public:
-    void sortColors(vector<int>& a) {
-       
-        int l = 0, r = a.size() - 1;
-        for(int i = 0; i <= r; i++) {
-            if(a[i] == 0) {
-                swap(a[i], a[l]);
-                l++;
-            } else if(a[i] ==2 ) {
-                swap(a[i], a[r]);
-                r--;
-                i--;
+    void sortColors(vector<int>& nums) {
+        int low=0;
+        int high=nums.size()-1;
+        for(int i=0;i<=high;){
+            if(nums[i]==0){
+                swap(nums[i],nums[low]);
+                i++;
+                low++;
+            }else if(nums[i]==1) i++;
+            else{
+                swap(nums[i],nums[high]);
+                high--;
             }
         }
     }
