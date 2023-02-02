0122c6fbae9ffc4926bebdf897cab41c003fda4c
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2023-02-03 02:07:04 +0530

    Time: 485 ms (41.82%), Space: 158 MB (22.24%) - LeetHub

--- 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
+++ 1376-time-needed-to-inform-all-employees/1376-time-needed-to-inform-all-employees.cpp
@@ -17,15 +17,13 @@ public:
     }
     
     int numOfMinutes(int n, int headID, vector<int>& manager, vector<int>& informTime) {
-        
-        // Fill umap
+       
         for(int i=0; i<n; i++){
             int val = manager[i];
             if(val != -1){
                 umap[val].push_back(i);
             }
-        }
-        
+        }  
         dfs(headID, informTime);
         return mx;
         
