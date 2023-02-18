985fedb7d9e66410684d5af53587748b0e55bc7f
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2023-02-19 00:50:14 +0530

    Time: 31 ms (88.54%), Space: 18.7 MB (49.05%) - LeetHub

--- 0986-interval-list-intersections/0986-interval-list-intersections.cpp
+++ 0986-interval-list-intersections/0986-interval-list-intersections.cpp
@@ -1,24 +1,25 @@
 class Solution {
 public:
-    vector<vector<int>> intervalIntersection(vector<vector<int>>& firstList, vector<vector<int>>& secondList) {
-        vector<vector<int>>ans;
-        int i =0;
-        int j =0;
+    vector<vector<int>> intervalIntersection(vector<vector<int>>& f, vector<vector<int>>& s) {
         
-        // 0 is the starting point
-        //1 is the ending point
-        while(i < firstList.size() && j < secondList.size()){
-            int st = max(firstList[i][0], secondList[j][0]);//check which has maximum starting point because that will be the starting point of any intersection
-            int et = min(firstList[i][1], secondList[j][1]);
-            //for ending point it will be the minimum of both the ending point 
-            if(st <= et){//case of interval intersection
-              ans.push_back({st, et}); // push into ans
-              
-            }
-            if(firstList[i][1] <= secondList[j][1]) i++;
-            //if ending point of firstList is smaller than secondlist ending point increment i 
-            else j++; // otherwise increment j
+        vector<vector<int>>ans;
+        
+        int i=0;
+        int j=0;
+        
+        while(i<f.size() and j<s.size()){
+            
+            int st=max(f[i][0],s[j][0]);
+            int et=min(f[i][1],s[j][1]);
+            
+            if(st<=et) ans.push_back({st,et});
+            
+            if(f[i][1]<=s[j][1]) i++;
+            else j++;
+            
         }
-    return ans;
+        
+        return ans;
+        
     }
 };
