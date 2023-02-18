637465c52c5bbb7a415a05e0edc1ffa7f8c8aeb4
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2023-02-18 19:11:47 +0530

    Time: 0 ms (100.00%), Space: 9.8 MB (7.06%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.cpp
+++ 0226-invert-binary-tree/0226-invert-binary-tree.cpp
@@ -16,7 +16,7 @@ public:
         swap(root->left,root->right);
         invertTree(root->left);
         invertTree(root->right);    
-                   return root;
+        return root;
         
     }
 };
