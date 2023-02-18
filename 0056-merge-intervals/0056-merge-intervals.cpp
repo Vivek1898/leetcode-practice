4a8a282b0571e8266f7d2c8fb80e7abf9fa89f54
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2023-02-19 00:03:28 +0530

    Time: 45 ms (51.20%), Space: 20.2 MB (17.94%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -1,31 +1,26 @@
 class Solution {
 public:
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
+       vector<vector<int>>ans;
+     //   if(intervals.size()==0) return ans;
+        sort(intervals.begin(),intervals.end());
+        vector<int>pre = intervals[0];
+    
+        for(auto i:intervals){
             
-        vector<vector<int>> mergedIntervals;
-        if(intervals.size()==0)
-            return mergedIntervals;
-        
-        sort(intervals.begin(),intervals.end());// sort
-        vector<int> pre_interval = intervals[0];
-        // 0 is starting point
-        //1 is ending point
-        
-        //treating as 1d
-        for(auto interval:intervals)
-        {
-            if(interval[0]<=pre_interval[1])// means there is overlapping of intervals
-            {
-                pre_interval[1]=max(interval[1],pre_interval[1]); // need to check which has max ending points hence we will make ending point of pre_interval to max
+            if(i[0]<=pre[1]){
+                pre[1]=max(i[1],pre[1]);
+            }else{
+                
+                ans.push_back(pre);
+                pre=i;
+                
             }
-            else
-            { // if there is no overlapping
-                mergedIntervals.push_back(pre_interval); // simplely push pre-interval to mergedintervals vector
-                pre_interval=interval;
-            }     
         }
         
-        mergedIntervals.push_back(pre_interval);
-        return mergedIntervals;
+        
+        ans.push_back(pre);
+        
+        return ans;
     }
 };
