9a95af0b265b46b831ab7bc668760b470b5596eb
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2023-02-22 15:21:38 +0530

    Time: 62 ms (66.66%), Space: 45.5 MB (5.75%) - LeetHub

--- 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
+++ 1011-capacity-to-ship-packages-within-d-days/1011-capacity-to-ship-packages-within-d-days.cpp
@@ -3,6 +3,7 @@ public:
     bool isPossible(vector<int>weights,int mid,int days){
          int cur = 0, requiredDays = 1;
           for (int weight : weights) {
+              
                 if (cur + weight > mid) {  
                     cur = 0;
                     requiredDays++;
@@ -14,10 +15,10 @@ public:
     int shipWithinDays(vector<int>& weights, int days) {
         int left = *max_element(weights.begin(), weights.end());  
         int right = accumulate(weights.begin(), weights.end(), 0); 
-        while (left < right) {
+        while (left <= right) {
             int mid = left + (right - left) / 2; 
             if (isPossible(weights,mid,days)) {  
-                right = mid;
+                right = mid-1;
             } else { 
                 left = mid + 1;
             }
