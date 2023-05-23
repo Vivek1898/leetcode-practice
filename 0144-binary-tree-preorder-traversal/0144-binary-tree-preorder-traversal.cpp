5b19bc4b27f776ccfb2dee3c1acd0e754eaeb3d7
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2023-05-23 21:15:36 +0530

    Time: 0 ms (100.00%), Space: 8.5 MB (15.69%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.cpp
@@ -14,6 +14,29 @@ private:
     std::vector<int> kids;
 public:
     std::vector<int> preorderTraversal(TreeNode* root) {
+        if(root==nullptr) return {};
+       stack<TreeNode*>s;
+        vector<int>ans;
+        s.push(root);
+        while(!s.empty()){
+            TreeNode* top=s.top();
+            s.pop();
+            ans.push_back(top->val);
+            if(top->right) s.push(top->right);
+            if(top->left) s.push(top->left);
+            
+        }
+        return ans;
+    }
+   
+};
+
+
+class SolutionRECURSION {
+private:
+    std::vector<int> kids;
+public:
+    std::vector<int> preorderTraversal(TreeNode* root) {
         getKid(root);
         return kids;
     }
