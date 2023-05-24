f45bb09779f07da286ffb2bdec8544ee7d90c33c
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2023-05-24 22:09:20 +0530

    Time: 3 ms (87.50%), Space: 16.5 MB (48.38%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.cpp
+++ 0101-symmetric-tree/0101-symmetric-tree.cpp
@@ -12,13 +12,13 @@
 class Solution {
 public:
     bool solve(TreeNode* r1,TreeNode* r2){
-        if(r1==nullptr and r2==nullptr) return true;
+        if(r1==nullptr or r2==nullptr) return r1==r2;
       
-        if(r1==nullptr or r2==nullptr or r1->val!=r2->val) return false;
+        if(r1->val!=r2->val) return false;
         
         return solve(r1->right,r2->left) and solve(r1->left , r2->right);
     }
     bool isSymmetric(TreeNode* root) {
-      return  solve(root->left,root->right);
+      return root==nullptr or solve(root->left,root->right);
     }
 };
