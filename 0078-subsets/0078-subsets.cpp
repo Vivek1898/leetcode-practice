24e213bb3ad922f5dcdcccf2796d7913ed1bc105
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2024-04-11 22:44:08 +0530

    Time: 4 ms (40.13%), Space: 8.2 MB (82.09%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,48 +1,25 @@
-//BFS
-class Solution2 {
-public:
-    vector<vector<int>> subsets(vector<int>& nums) {
-        vector<vector<int>>powerset;
-        powerset.push_back({});
-        for(auto currentNumber:nums){
-            int n=powerset.size();
-            for(int i=0;i<n;i++){
-                // duplicates + new
-                  vector<int>copy(powerset[i]);
-                    copy.push_back(currentNumber);
-                        powerset.push_back(copy);
-            }
-            
-        }
-        return powerset;
-        
-    }
-};
-
-//Recursion
-
 class Solution {
 public:
-    void helper(vector<int>& nums,  vector<int> &curr,vector<vector<int>> &ans,int idx){
-        if(idx==nums.size()) {
-            ans.push_back(curr);
-            return;
-        }
-        
-        // pick and add
-        curr.push_back(nums[idx]);
-         helper(nums,curr,ans,idx+1);
-        curr.pop_back();
-        // not pick
+     void helper (vector<int>& nums , vector<int>&curr ,  vector<vector<int>>&ans , int idx){
+         if(idx==nums.size())
+         {
+             ans.push_back(curr);
+             return;
+         }
+         // pick copy and add
+         curr.push_back(nums[idx]);
          helper(nums,curr,ans,idx+1);
         
-    }
-    
+         // dont pick
+          curr.pop_back();
+             helper(nums,curr,ans,idx+1);
+     }
     vector<vector<int>> subsets(vector<int>& nums) {
-       vector<vector<int>> ans;
-         vector<int> curr;
-        helper(nums,curr,ans,0);
-        return ans;
+        vector<vector<int>>ans;
+        vector<int>curr;
+       helper(nums,curr,ans,0);
+         return ans;
+        
         
     }
 };
