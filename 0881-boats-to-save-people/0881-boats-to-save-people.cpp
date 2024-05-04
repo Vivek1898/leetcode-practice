edc40957eb7c212733d92fe5b2c73d80fc6aa5f2
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2024-05-04 20:47:11 +0530

    Time: 60 ms (40.93%), Space: 45.5 MB (46.86%) - LeetHub

--- 0881-boats-to-save-people/0881-boats-to-save-people.cpp
+++ 0881-boats-to-save-people/0881-boats-to-save-people.cpp
@@ -1,71 +1,24 @@
-class Solution2 {
+class Solution {
 public:
     int numRescueBoats(vector<int>& p, int limit) {
         
+        int n=p.size()-1;
+        int i=0 , j=n;
+        int ans=0;
         sort(p.begin(),p.end());
-        int low=0;
-        int high=p.size()-1;
-        int count=0;
-        while(low<=high){
-            int sum=p[low]+p[high];
-            
-            if(sum<=limit){
-                low++;
-                high--;
-                
-            }else{
-                high--;
-            }
-            count++;
-            
-        }
-        return count;
-    }
-};
-
-class Solution {
-    public:
-    
-bool check(int mid,vector<int>people,int limit)
-{
-int i = 0, j = people.size() - 1,cnt = 0;
-
-    while(i <= j)
-    {  
-        if(people[i] + people[j] <= limit)
-        {
-            ++i;
-            --j;
-        }
-        else
-        {
-            --j;
-        }
         
-        ++cnt;  // number of boats
-    }
-    
-    return cnt<=mid;
-}
-int numRescueBoats(vector<int>& people, int limit) {
-    sort(people.begin(),people.end());
-    int low=0;
-    int ans=0;
-    int high=people.size();
-    while(low<=high)
-    {
-        int mid=(low+high)/2;
-        if(check(mid,people,limit)==true)
-        {
-            ans=mid;
-            high=mid-1;
+        while(i<=j){
+            if(p[i]+p[j] <= limit){
+                i++;
+                j--;
+            }else{
+                 j--;
+            }
+           
+            ans++;
+           
         }
-        else
-        {
-            low=mid+1;
-        }
+        return ans;
+        
     }
-    return ans;
-   
-}
 };
