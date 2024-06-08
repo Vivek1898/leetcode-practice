aa8349d9217f8c90f891569bdffca8828e4fce1b
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2024-06-08 18:19:07 +0530

    Time: 4 ms (32.92%), Space: 9.4 MB (48.91%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.cpp
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -1,33 +1,26 @@
 class Solution {
 public:
-    int houseRob(vector<int>& num){
-        int n=num.size();
-        int dp[n];
-        dp[0]=num[0];
-        dp[1]=max(num[0],num[1]);
-        for(int i=2;i<n;i++){
-            dp[i]=max(dp[i-1],dp[i-2]+num[i]);
-        }
-        return dp[n-1];
-        
-    }
     int rob(vector<int>& nums) {
-        // minor cases
-          int n=nums.size();
-        if(n==0) return 0;
-        if(n==1) return nums[0];
-        if(n==2) return max(nums[0],nums[1]);
+        int n = nums.size();
+        if (n == 1) return nums[0];
+        if (n == 2) return max(nums[0], nums[1]);
         
-        // two choics either use 
-        // 1st house OR
+        // Rob houses from 
+        // 0 to n-2 and 
+        // 1 to n-1
+        vector<int> dp1(n, -1);
+        vector<int> dp2(n, -1);
+        return max(memo(0, nums, dp1, n-1), memo(1, nums, dp2, n));
+    }
+
+private:
+    int memo(int i, vector<int>& v, vector<int>& dp, int end) {
+        if (i >= end) return 0;
+        if (dp[i] != -1) return dp[i];
+
+        int robAndMove = v[i] + memo(i + 2, v, dp, end);
+        int move = memo(i + 1, v, dp, end);
         
-        // Second House
-         vector<int> v1(nums.begin(), nums.end()-1);
-        vector<int> v2(nums.begin()+1, nums.end());
-        
-        int ans=max(houseRob(v1), houseRob(v2));
-        return ans;
-        
-        
+        return dp[i] = max(robAndMove, move);
     }
 };
