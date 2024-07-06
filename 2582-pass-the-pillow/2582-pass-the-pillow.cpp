77912d8244d9021081007a65fca0cd1d85b8e8e9
Vivek Singh <72497717+Vivek1898@users.noreply.github.com>
2024-07-06 08:08:22 +0530

    Time: 2 ms (54.14%), Space: 7.1 MB (71.18%) - LeetHub

---
+++ 2582-pass-the-pillow/2582-pass-the-pillow.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int passThePillow(int n, int time) {
+        if(n>time) return ++time;
+        int res=1,i=1;
+        bool flag=true;
+        while(i<=time){
+            if(res==1)  flag=true;
+            if(res==n)  flag=false;
+            flag?res++:res--;
+            i++;
+        }
+        return res;
+    }
+};
